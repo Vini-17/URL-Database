1b7937c1a308a8821d0801c64cf1b3a5fe5cd5ea
Vini-17 <121810552+Vini-17@users.noreply.github.com>
2023-12-22 11:23:50 +0530

    Update UrlDatabase.java
    Refactored

--- UrlDatabase.java
+++ UrlDatabase.java
@@ -3,96 +3,6 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class UrlDatabase {
-    private static class UrlData {
-        private String url;
-        private int count;
-
-        public UrlData(String url, int count) {
-            this.url = url;
-            this.count = count;
-        }
-
-        public String getUrl() {
-            return url;
-        }
-
-        public int getCount() {
-            return count;
-        }
-
-        public void incrementCount() {
-            count++;
-        }
-
-        public void setUrl(String url) {
-            this.url = url;
-        }
-    }
-
-    private static void storeUrl(String url) {
-        String shortKey = "url" + counter++;
-        UrlData urlData = new UrlData(url, 0);
-        urlMap.put(shortKey, urlData);
-        System.out.println("URL stored with short key: " + shortKey);
-    }
-
-    private static void getUrl(String url) {
-        for (Map.Entry<String, UrlData> entry : urlMap.entrySet()) {
-            if (entry.getValue().getUrl().equals(url)) {
-                entry.getValue().incrementCount();
-                System.out.println("Short key for " + url + ": " + entry.getKey());
-                return;
-            }
-        }
-        System.out.println("URL not found.");
-    }
-
-    private static void getCount(String url) {
-        for (UrlData urlData : urlMap.values()) {
-            if (urlData.getUrl().equals(url)) {
-                System.out.println("Usage count for " + url + ": " + urlData.getCount());
-                return;
-            }
-        }
-        System.out.println("URL not found.");
-    }
-
-    private static void listUrls() {
-        System.out.println("URLs and counts in the database:");
-        for (Map.Entry<String, UrlData> entry : urlMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue().getUrl() +
-                    " (Usage Count: " + entry.getValue().getCount() + ")");
-        }
-    }
-
-    private static void getUrlByShortKey(String shortKey) {
-        UrlData urlData = urlMap.get(shortKey);
-        if (urlData != null) {
-            System.out.println("URL for short key " + shortKey + ": " + urlData.getUrl());
-        } else {
-            System.out.println("Short key not found.");
-        }
-    }
-
-    private static void deleteUrlByShortKey(String shortKey) {
-        if (urlMap.containsKey(shortKey)) {
-            urlMap.remove(shortKey);
-            System.out.println("URL with short key " + shortKey + " deleted.");
-        } else {
-            System.out.println("Short key not found.");
-        }
-    }
-
-    private static void updateUrlByShortKey(String shortKey, String newUrl) {
-        if (urlMap.containsKey(shortKey)) {
-            UrlData urlData = urlMap.get(shortKey);
-            urlData.setUrl(newUrl);
-            System.out.println("URL with short key " + shortKey + " updated to: " + newUrl);
-        } else {
-            System.out.println("Short key not found.");
-        }
-    }
-
 
     private static Map<String, UrlData> urlMap = new HashMap<>();
     private static int counter = 1;
@@ -174,6 +84,93 @@ public class UrlDatabase {
         System.out.println("Exiting URL Database. Goodbye!");
     }
 
-   
+    private static void storeUrl(String url) {
+        String shortKey = "url" + counter++;
+        UrlData urlData = new UrlData(url, 0);
+        urlMap.put(shortKey, urlData);
+        System.out.println("URL stored with short key: " + shortKey);
+    }
 
+    private static void getUrl(String url) {
+        for (Map.Entry<String, UrlData> entry : urlMap.entrySet()) {
+            if (entry.getValue().getUrl().equals(url)) {
+                entry.getValue().incrementCount();
+                System.out.println("Short key for " + url + ": " + entry.getKey());
+                return;
+            }
+        }
+        System.out.println("URL not found.");
+    }
+
+    private static void getCount(String url) {
+        for (UrlData urlData : urlMap.values()) {
+            if (urlData.getUrl().equals(url)) {
+                System.out.println("Usage count for " + url + ": " + urlData.getCount());
+                return;
+            }
+        }
+        System.out.println("URL not found.");
+    }
+
+    private static void listUrls() {
+        System.out.println("URLs and counts in the database:");
+        for (Map.Entry<String, UrlData> entry : urlMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue().getUrl() +
+                    " (Usage Count: " + entry.getValue().getCount() + ")");
+        }
+    }
+
+    private static void getUrlByShortKey(String shortKey) {
+        UrlData urlData = urlMap.get(shortKey);
+        if (urlData != null) {
+            System.out.println("URL for short key " + shortKey + ": " + urlData.getUrl());
+        } else {
+            System.out.println("Short key not found.");
+        }
+    }
+
+    private static void deleteUrlByShortKey(String shortKey) {
+        if (urlMap.containsKey(shortKey)) {
+            urlMap.remove(shortKey);
+            System.out.println("URL with short key " + shortKey + " deleted.");
+        } else {
+            System.out.println("Short key not found.");
+        }
+    }
+
+    private static void updateUrlByShortKey(String shortKey, String newUrl) {
+        if (urlMap.containsKey(shortKey)) {
+            UrlData urlData = urlMap.get(shortKey);
+            urlData.setUrl(newUrl);
+            System.out.println("URL with short key " + shortKey + " updated to: " + newUrl);
+        } else {
+            System.out.println("Short key not found.");
+        }
+    }
+
+    private static class UrlData {
+        private String url;
+        private int count;
+
+        public UrlData(String url, int count) {
+            this.url = url;
+            this.count = count;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        public void incrementCount() {
+            count++;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+    }
 }
